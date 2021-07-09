3b7c88f511c01d3a8600500bed74380d619ed071
Laura Izabel <lauraizabel8569@gmail.com>
2021-07-09 16:59:07 -0300

    adding typography

---
+++ src/@types/index.ts
@@ -0,0 +1,18 @@
+export interface ComponentStructure {
+  label: string | null;
+  componentId: string | null;
+  type: string | null;
+  helpBlock: string | null;
+  order: string | null;
+  options: string[] | null;
+  minimum: {
+    type: string | null;
+    value: number | null;
+  };
+  maximum: number | null;
+  widget: string | null;
+}
+
+export interface FormStructure extends ComponentStructure {
+  components: ComponentStructure | null;
+}
