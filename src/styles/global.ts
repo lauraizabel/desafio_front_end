5a1e2719f6cd7342a253de8737d51336983c7cca
Laura Izabel <lauraizabel8569@gmail.com>
2021-07-12 23:10:27 -0300

    adding new font "Roboto"

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
   * {
     margin: 0;
     padding: 0;
+    font-family: 'Roboto', sans-serif;
   }
   body {
     width: 100%;
     height: 100%;
     background-color: #443742;
+  }
+  html, #root {
+    height: 100% !important;
   }
 `;
 
